247f2fcf44b129de506fd7b7945921d3739f6192
Jan Mrkos <mrkosjan@gmail.com>
2024-05-07 16:43:29 +0200

    Updated Geocoder dosctring.

--- src/geocoder.jl
+++ src/geocoder.jl
@@ -59,9 +59,15 @@ const DEFAULT_DECODER_OUTPUT  = [:country_code, :name, :country]
 
 """
 Geocoder(cities_data::AbstractDataFrame; filters::Vector{Function} = Function[])
-Geocoder(;data_dir::String=DATA_DIR, geo_file::String=GEO_FILE, filters::Vector{Function} = Function[])
+Geocoder(;
+    data_dir::String=DATA_DIR, 
+    geo_file::String=GEO_FILE, 
+    decoder_output_columns::Vector{Symbol} = DEFAULT_DECODER_OUTPUT,
+    filters::Vector{Function} = Function[])
 
 Geocoder structure that holds the reference points and their labels (city name and country code).
+
+The second constructor is used to create the Geocoder from the geoname data that it automatically downloads on first run.
 """
 struct Geocoder
     tree::NNTree
